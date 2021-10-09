8dffe27210c3400d0eb4fc7e730be3a9ae9c1e51
Marijana <Marijana@remote1>
2021-10-09 13:11:42 +0200

    Initial commit

---
+++ app/index.php
@@ -0,0 +1,9 @@
+<?php
+namespace app;
+
+use app\navLinks;
+
+require __DIR__.'/../vendor/autoload.php';
+include_once 'templates/nav_links.php';
+//$nav = new  navLinks();
+//echo $nav;
---
+++ app/templates/nav_links.php
@@ -0,0 +1,25 @@
+<?php
+/*namespace app;
+
+class navLinks{
+
+    public $login;
+    protected $register;
+    public $search;
+
+    public function __construct(){
+        $this->login ='<a src="*">Loging</a>';
+        $this->register = '<a src="*">Register</a>';
+        $this->search= '<a src="*">Search</a>';
+
+        return [$this->login, $this->register, $this->search];
+    }
+}*/
+?>
+<div class="container">
+    <div class="col-md-12">
+        <a href="*">Login</a>
+        <a href="*">Register</a>
+        <a href="*">Register</a>
+    </div>
+</div>
